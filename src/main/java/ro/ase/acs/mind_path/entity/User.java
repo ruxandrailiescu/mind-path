830901d1e949598f3304ba5d3f207fe2638d620f
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2025-03-19 17:18:07 +0200

    User class implements UserDetails

--- src/main/java/ro/ase/acs/mind_path/entity/User.java
+++ src/main/java/ro/ase/acs/mind_path/entity/User.java
@@ -3,16 +3,19 @@ package ro.ase.acs.mind_path.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 import ro.ase.acs.mind_path.entity.enums.UserRole;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "Users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -34,4 +37,34 @@ public class User {
     private List<QuizAttempt> quizAttempts;
     @OneToMany(mappedBy = "createdBy", cascade = CascadeType.ALL)
     private List<Quiz> quizzes;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of((GrantedAuthority) () -> "ROLE_" + getRole().toString());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
