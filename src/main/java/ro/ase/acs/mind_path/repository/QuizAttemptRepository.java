afdb99105a79be029b72f7dcdc5c596f14b68c7a
ruxandrailiescu <iliescuruxandra23@stud.ase.ro>
2025-03-21 15:30:47 +0200

    Modifying application.properties

--- src/main/java/ro/ase/acs/mind_path/repository/QuizAttemptRepository.java
+++ src/main/java/ro/ase/acs/mind_path/repository/QuizAttemptRepository.java
@@ -2,7 +2,8 @@ package ro.ase.acs.mind_path.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ro.ase.acs.mind_path.entity.QuizAttempt;
 
 @Repository
-public interface QuizAttemptRepository extends JpaRepository<QuizAttemptRepository, Long> {
+public interface QuizAttemptRepository extends JpaRepository<QuizAttempt, Long> {
 }
