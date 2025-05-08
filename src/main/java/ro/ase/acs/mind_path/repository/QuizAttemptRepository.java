b40dc22f9935ad42b7f948ab26e69621ac51742b
ruxandrailiescu <iliescuruxandra23@stud.ase.ro>
2025-05-08 04:31:50 +0300

    Repositories for functionality: submit quiz attempt

--- src/main/java/ro/ase/acs/mind_path/repository/QuizAttemptRepository.java
+++ src/main/java/ro/ase/acs/mind_path/repository/QuizAttemptRepository.java
@@ -2,8 +2,14 @@ package ro.ase.acs.mind_path.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ro.ase.acs.mind_path.entity.Quiz;
 import ro.ase.acs.mind_path.entity.QuizAttempt;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface QuizAttemptRepository extends JpaRepository<QuizAttempt, Long> {
+    List<QuizAttempt> findByUserUserIdAndQuizQuizId(Long userId, Long quizId);
+    Optional<QuizAttempt> findByAttemptIdAndUserUserId(Long attemptId, Long userId);
 }
--- src/main/java/ro/ase/acs/mind_path/repository/UserResponseRepository.java
+++ src/main/java/ro/ase/acs/mind_path/repository/UserResponseRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ro.ase.acs.mind_path.entity.UserResponse;
 
+import java.util.List;
+
 @Repository
 public interface UserResponseRepository extends JpaRepository<UserResponse, Long> {
+    List<UserResponse> findByQuizAttemptAttemptId(Long attemptId);
 }
